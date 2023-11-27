ec0cde276632db300d719aa9ccf9f175d9889110
Saturnstrings <144817297+Saturnstrings@users.noreply.github.com>
2023-11-27 10:39:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$("button").click(function() {
+$("yes-button").show("Part-Two-Yes");
+});
+
+$("button").click(function() {
+  $("no-button").show("Part-Two-No");
+});
+
+$(".part").click(function() {
+  $("forest-img").show("Story-Ending-Yes");
+});
+
+$(".part").click(function() {
+  $("forest-img2").show("Story-Ending-No");
+});
+
